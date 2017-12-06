64ce6417716ad5c50958f00c7bbdcad5b0b962ce
Kevin Barnes <kbarnes3@gmail.com>
2017-12-06 13:51:21 -0800

    Parsing numbers

--- AdventOfCode2017/InverseCaptcha/InverseCaptcha.cpp
+++ AdventOfCode2017/InverseCaptcha/InverseCaptcha.cpp
@@ -3,6 +3,34 @@
 
 #include "stdafx.h"
 
+int char_to_int(wchar_t c)
+{
+    switch (c)
+    {
+        case L'0':
+            return 0;
+        case L'1':
+            return 1;
+        case L'2':
+            return 2;
+        case L'3':
+            return 3;
+        case L'4':
+            return 4;
+        case L'5':
+            return 5;
+        case L'6':
+            return 6;
+        case L'7':
+            return 7;
+        case L'8':
+            return 8;
+        case L'9':
+            return 9;
+        default:
+            return -1;
+    }
+}
 
 int main()
 {
@@ -12,7 +40,8 @@ int main()
 
     for (wchar_t& c : input)
     {
-        wprintf(L"%c", c);
+        int number = char_to_int(c);
+        wprintf(L"%d", number);
     }
     wprintf(L"\n");
 
