aa8c67b92bdf985e26350403ceea2224c1354b6f
Kevin Barnes <kbarnes3@gmail.com>
2017-12-15 18:18:31 -0800

    Trying to add a boost tokenizer

--- AdventOfCode2017/CorruptionChecksum/CorruptionChecksum.cpp
+++ AdventOfCode2017/CorruptionChecksum/CorruptionChecksum.cpp
@@ -2,16 +2,18 @@
 //
 
 #include "stdafx.h"
-
+using namespace boost;
 
 int main()
 {
     std::wstring row;
+    char_separator<wchar_t> tab_sep(L"\t");
     while (std::getline(std::wcin, row))
     {
         if (row.length() > 0)
         {
             wprintf(L"Row: %s\n", row);
+            //tokenizer<char_separator<wchar_t>> tokens(row, tab_sep);
         }
     }
     return 0;
