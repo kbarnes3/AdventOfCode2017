8cd24b55003f426d03b3a1f6c25c7df7f175a5fa
Kevin Barnes <kbarnes3@gmail.com>
2017-12-18 18:27:02 -0800

    Adding some math

--- AdventOfCode2017/SpiralMemory/SpiralMemory.cpp
+++ AdventOfCode2017/SpiralMemory/SpiralMemory.cpp
@@ -4,8 +4,24 @@
 #include "stdafx.h"
 
 
+long int ringForNumber(long int target)
+{
+    // The ring is the square root of the target, minus 1, divided by 2, then rounded up
+    double targetDouble = target;
+
+    return static_cast<long int>(ceil(targetDouble));
+}
+
+
 int main()
 {
+    std::wstring row;
+    std::getline(std::wcin, row);
+
+    long int target = wcstol(row.c_str(), nullptr, 10);
+
+    wprintf(L"%d is at (%d, %d)", target, 0, 0);
+
     return 0;
 }
 
